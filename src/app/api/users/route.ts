fac1b4dd9a789662c1ddab3105799c9a6da5c685
Muhammad Bilal <mbilal@mbilal-mac.home>
2024-06-25 01:21:32 +0100

    add connection with db with mongoose

---
+++ src/app/api/users/route.ts
@@ -0,0 +1,17 @@
+import { NextResponse } from "next/server";
+import connectDB from "@/configs/dbConfig";
+
+connectDB();
+
+export async function GET() {
+    return NextResponse.json({
+        success: true,
+        data: [
+            {
+                id: 1,
+                name: 'John Doe',
+            }
+        ]
+    });
+
+}
